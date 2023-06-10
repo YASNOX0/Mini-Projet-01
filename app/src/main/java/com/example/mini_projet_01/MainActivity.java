9c6866cf721ae078b1b7ce59b37c7882527923e2
DESKTOP-YAS\ULTRAPC <yassinsaaden@gmail.com>
2023-06-10 15:29:24 +0100

    Read from Raw folder

--- app/src/main/java/com/example/mini_projet_01/MainActivity.java
+++ app/src/main/java/com/example/mini_projet_01/MainActivity.java
@@ -2,9 +2,14 @@ package com.example.mini_projet_01;
 
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+import java.io.IOException;
+import java.io.InputStream;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
 
     Button btn_loadUsers , btn_quit;
     @Override
@@ -13,6 +18,23 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         btn_loadUsers = findViewById(R.id.btn_loadUsers);
         btn_quit = findViewById(R.id.btn_quit);
+
+        btn_loadUsers.setOnClickListener(this);
+        btn_quit.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+        if(view.equals(btn_loadUsers)){
+            InputStream inputStream = getResources().openRawResource(R.raw.users);
+            try {
+                Toast.makeText(this, Character.toString((char) inputStream.read()), Toast.LENGTH_SHORT).show();
+            }catch(IOException e){
+                e.printStackTrace();
+            }
+        }else if(view.equals(btn_quit)){
+            finish();
+        }
 
     }
 }
