edabb585a19eb48bd1a64fbb65c143e3dd4caa83
DESKTOP-YAS\ULTRAPC <yassinsaaden@gmail.com>
2023-06-10 15:59:47 +0100

    Read JSON file content as String from assets Folder

--- app/src/main/java/com/example/mini_projet_01/MainActivity.java
+++ app/src/main/java/com/example/mini_projet_01/MainActivity.java
@@ -1,6 +1,7 @@
 package com.example.mini_projet_01;
 
 import androidx.appcompat.app.AppCompatActivity;
+
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -9,9 +10,10 @@ import android.widget.Toast;
 import java.io.IOException;
 import java.io.InputStream;
 
-public class MainActivity extends AppCompatActivity implements View.OnClickListener{
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
-    Button btn_loadUsers , btn_quit;
+    Button btn_loadUsers, btn_quit;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -25,14 +27,25 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
     @Override
     public void onClick(View view) {
-        if(view.equals(btn_loadUsers)){
-            InputStream inputStream = getResources().openRawResource(R.raw.users);
-            try {
-                Toast.makeText(this, Character.toString((char) inputStream.read()), Toast.LENGTH_SHORT).show();
-            }catch(IOException e){
+        if (view.equals(btn_loadUsers)) {
+            try (InputStream inputStream = getAssets().open("users.json")) {
+
+                int code;
+                StringBuilder stringBuilder = new StringBuilder();
+                String jsonString;
+
+                code = inputStream.read();
+                while (code != -1) {
+                    stringBuilder.append((char) code);
+                    code = inputStream.read();
+                }
+                jsonString = stringBuilder.toString();
+
+                Toast.makeText(this, jsonString, Toast.LENGTH_SHORT).show();
+            } catch (IOException e) {
                 e.printStackTrace();
             }
-        }else if(view.equals(btn_quit)){
+        } else if (view.equals(btn_quit)) {
             finish();
         }
 
