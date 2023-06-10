304fc4668a49191efde0a196717fcf2dd8cab007
DESKTOP-YAS\ULTRAPC <yassinsaaden@gmail.com>
2023-06-10 19:42:17 +0100

    Display users in simple ListView

--- app/src/main/java/com/example/mini_projet_01/MainActivity.java
+++ app/src/main/java/com/example/mini_projet_01/MainActivity.java
@@ -4,7 +4,9 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.ListView;
 import android.widget.Toast;
 
 import org.json.JSONArray;
@@ -13,10 +15,12 @@ import org.json.JSONObject;
 
 import java.io.IOException;
 import java.io.InputStream;
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
     Button btn_loadUsers, btn_quit;
+    ListView lv_users;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,6 +28,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         setContentView(R.layout.activity_main);
         btn_loadUsers = findViewById(R.id.btn_loadUsers);
         btn_quit = findViewById(R.id.btn_quit);
+        lv_users = findViewById(R.id.lv_users);
 
         btn_loadUsers.setOnClickListener(this);
         btn_quit.setOnClickListener(this);
@@ -32,36 +37,44 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     @Override
     public void onClick(View view) {
         if (view.equals(btn_loadUsers)) {
-            try (InputStream inputStream = getAssets().open("users.json")) {
-
-                int code;
-                StringBuilder stringBuilder = new StringBuilder();
-                String jsonString;
-
-                code = inputStream.read();
-                while (code != -1) {
-                    stringBuilder.append((char) code);
-                    code = inputStream.read();
-                }
-                jsonString = stringBuilder.toString();
-
-                JSONObject jsonObject = new JSONObject(jsonString);
-                JSONArray jsonArray = jsonObject.getJSONArray("users");
-                StringBuilder stringBuilderFullName = new StringBuilder();
-                for (int i = 0; i < jsonArray.length(); i++) {
-                    JSONObject user = jsonArray.getJSONObject(i);
-                    JSONObject userName  = user.getJSONObject("name");
-                    String fullName = String.format("%s %s%n", userName.get("first"), userName.get("last"));
-
-                    stringBuilderFullName.append(fullName);
-                }
-                Toast.makeText(this , stringBuilderFullName , Toast.LENGTH_SHORT).show();
-            } catch (IOException | JSONException e) {
-                e.printStackTrace();
-            }
+            ArrayAdapter<String> adapter = new ArrayAdapter<>(this,
+                    android.R.layout.simple_list_item_1,
+                    getUsers());
+            lv_users.setAdapter(adapter);
         } else if (view.equals(btn_quit)) {
             finish();
         }
 
+    }
+
+    private ArrayList<String> getUsers() {
+
+        ArrayList<String> userFullNames = new ArrayList<>();
+
+        try (InputStream inputStream = getAssets().open("users.json")) {
+
+            int code;
+            StringBuilder stringBuilder = new StringBuilder();
+            String jsonString;
+
+            code = inputStream.read();
+            while (code != -1) {
+                stringBuilder.append((char) code);
+                code = inputStream.read();
+            }
+            jsonString = stringBuilder.toString();
+
+            JSONObject jsonObject = new JSONObject(jsonString);
+            JSONArray jsonArray = jsonObject.getJSONArray("users");
+            for (int i = 0; i < jsonArray.length(); i++) {
+                JSONObject user = jsonArray.getJSONObject(i);
+                JSONObject userName = user.getJSONObject("name");
+
+                userFullNames.add(String.format("%s %s%n", userName.get("first"), userName.get("last")));
+            }
+        } catch (IOException | JSONException e) {
+            e.printStackTrace();
+        }
+        return userFullNames;
     }
 }
