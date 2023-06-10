44f987a236ea09c46cb2dfedba9e788af5fa4a39
DESKTOP-YAS\ULTRAPC <yassinsaaden@gmail.com>
2023-06-10 18:09:41 +0100

    Display usrers full name  inin Toast

--- app/src/main/java/com/example/mini_projet_01/MainActivity.java
+++ app/src/main/java/com/example/mini_projet_01/MainActivity.java
@@ -7,6 +7,10 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
 import java.io.IOException;
 import java.io.InputStream;
 
@@ -41,8 +45,18 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                 }
                 jsonString = stringBuilder.toString();
 
-                Toast.makeText(this, jsonString, Toast.LENGTH_SHORT).show();
-            } catch (IOException e) {
+                JSONObject jsonObject = new JSONObject(jsonString);
+                JSONArray jsonArray = jsonObject.getJSONArray("users");
+                StringBuilder stringBuilderFullName = new StringBuilder();
+                for (int i = 0; i < jsonArray.length(); i++) {
+                    JSONObject user = jsonArray.getJSONObject(i);
+                    JSONObject userName  = user.getJSONObject("name");
+                    String fullName = String.format("%s %s%n", userName.get("first"), userName.get("last"));
+
+                    stringBuilderFullName.append(fullName);
+                }
+                Toast.makeText(this , stringBuilderFullName , Toast.LENGTH_SHORT).show();
+            } catch (IOException | JSONException e) {
                 e.printStackTrace();
             }
         } else if (view.equals(btn_quit)) {
