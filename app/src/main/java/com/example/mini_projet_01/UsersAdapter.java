911ed3c9441ae6f55d56bbd13cfede132385b424
DESKTOP-YAS\ULTRAPC <yassinsaaden@gmail.com>
2023-06-11 02:18:15 +0100

    Display users in simple ListView - Create Custom Adapter(UserAdapter) - first Step

---
+++ app/src/main/java/com/example/mini_projet_01/UsersAdapter.java
@@ -0,0 +1,41 @@
+package com.example.mini_projet_01;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+
+import java.util.ArrayList;
+
+public class UsersAdapter extends BaseAdapter {
+
+    Context context;
+    private ArrayList<User> users;
+
+    public UsersAdapter(Context context, ArrayList<User> users) {
+        this.users = users;
+        this.context = context;
+    }
+
+    @Override
+    public int getCount() {
+        return users.size();
+    }
+
+    @Override
+    public Object getItem(int i) {
+        return users.get(i);
+    }
+
+    @Override
+    public long getItemId(int i) {
+        return 0;
+    }
+
+    @Override
+    public View getView(int i, View view, ViewGroup viewGroup) {
+        view = LayoutInflater.from(context).inflate(R.layout.item_user , null);
+        return view;
+    }
+}
