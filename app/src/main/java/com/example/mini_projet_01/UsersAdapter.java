d79c4e740fc05421b57ef7f3758c3484a14b0ca3
DESKTOP-YAS\ULTRAPC <yassinsaaden@gmail.com>
2023-06-11 02:53:45 +0100

    ex02 has been solved

--- app/src/main/java/com/example/mini_projet_01/UsersAdapter.java
+++ app/src/main/java/com/example/mini_projet_01/UsersAdapter.java
@@ -1,11 +1,14 @@
 package com.example.mini_projet_01;
 
 import android.content.Context;
+import android.graphics.Color;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
 import android.widget.TextView;
+
+import androidx.constraintlayout.widget.ConstraintLayout;
 
 import java.util.ArrayList;
 
@@ -41,11 +44,17 @@ public class UsersAdapter extends BaseAdapter {
         TextView tv_itemUserFullName = view.findViewById(R.id.tv_itemUserFullName);
         TextView tv_itemUserCity = view.findViewById(R.id.tv_itemUserCity);
         TextView tv_itemUserGender = view.findViewById(R.id.tv_itemUserGender);
+        ConstraintLayout cl_itemUser = view.findViewById(R.id.cl_itemUser);
 
         tv_itemUserFullName.setText(users.get(i).fullName());
         tv_itemUserCity.setText(users.get(i).getCity());
-        tv_itemUserGender.setText(users.get(i).getGender());
+        tv_itemUserGender.setText(String.format("#%d",i+1));
 
+        if(users.get(i).getGender().equals("male")){
+            cl_itemUser.setBackgroundColor(Color.rgb(137, 207, 240));
+        }else {
+            cl_itemUser.setBackgroundColor(Color.rgb(255, 192, 203));
+        }
         return view;
     }
 }
